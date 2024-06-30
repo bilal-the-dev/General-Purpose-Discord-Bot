ee9c6f68f549fe4ccea594364679ea109f4b15f1
Bilal <bilalfarooq6001@gmail.com>
2024-06-30 14:48:36 +0500

    ddelete extra cmds

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -2,5 +2,6 @@ require("dotenv").config();
 
 module.exports = (client) => {
 	client.application.commands.set([]);
+	client.guilds.cache.get("1218982026330243143").commands.set([]);
 	console.log(`${client.user.tag} is online.`);
 };
